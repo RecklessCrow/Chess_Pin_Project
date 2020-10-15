be2f392a39971952984220e2ffe9f95723c31cf0
CJ <jmcc2646@gmail.com>
2020-10-15 07:00:11 -0400

    initial commit

--- pin.py
+++ pin.py
@@ -25,6 +25,51 @@ for letter in 'ABCDEFGH':
 BLACK_SQUARES.sort()
 
 
+class Graph:
+    def __init__(self, num_nodes):
+        self.nodes = {i + 1: [] for i in range(num_nodes)}
+
+    def __str__(self):
+        return str(self.nodes)
+
+    def neighbors(self, node):
+        return self.nodes[node]
+
+    def add_edge(self, start, end, cost):
+        self.nodes[end].append((start, cost))
+
+
+def bfs(graph, start, goal):
+    # keep track of unvisited nodes, the path taken, and the cost of the path
+    open_list = deque()
+    closed_list = {}
+    visited = [goal]
+    open_list.append((goal, 0, visited))
+    path = []
+    path_cost = float('-inf')
+
+    while open_list:
+        current_node, current_cost, current_visited = open_list.pop()
+
+        for next_node, cost in graph.neighbors(current_node):
+
+            new_cost = cost ** current_cost if current_cost != 0 else cost
+
+            if next_node not in closed_list or new_cost > closed_list[next_node]:
+                closed_list[next_node] = new_cost
+                open_list.append((next_node, new_cost, current_visited + [next_node]))
+
+            if next_node == start:
+                # todo: check for lexicographical order if costs are equal
+                if new_cost > path_cost:
+                    path = current_visited + [next_node]
+                    path_cost = new_cost
+
+    path.reverse()
+
+    return path, path_cost
+
+
 def calculate_pin(black, white):
     board = chess.Board()
     board.clear()
